f60f3b15373afd3bff67b276debdd65182acc9dc
evqzevqz <anaclaraevequoz4@gmail.com>
2025-08-14 12:35:00 -0300

    listo ej 2

--- firmware/projects/ejer1/main/x_template.c
+++ firmware/projects/ejer1/main/x_template.c
@@ -1,17 +1,8 @@
-/*! @mainpage Template
+/*! @mainpage Blinking switch
  *
- * @section genDesc General Description
+ * \section genDesc General Description
  *
- * This section describes how the program works.
- *
- * <a href="https://drive.google.com/...">Operation Example</a>
- *
- * @section hardConn Hardware Connection
- *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
- *
+ * This example makes LED_1 and LED_2 blink if SWITCH_1 or SWITCH_2 are pressed.
  *
  * @section changelog Changelog
  *
@@ -26,14 +17,36 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
 /*==================[macros and definitions]=================================*/
-
+#define CONFIG_BLINK_PERIOD 1000
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	uint8_t teclas;
+	LedsInit();
+	SwitchesInit();
+    while(1)    {
+    	teclas  = SwitchesRead();
+    	switch(teclas){
+    		case SWITCH_1:
+    			LedToggle(LED_1);
+    		break;
+    		case SWITCH_2:
+    			LedToggle(LED_2);
+    		break;
+    		case (SWITCH_1 | SWITCH_2):  // Ambos a la vez
+			LedToggle(LED_3);
+			break;
+    	}
+	    //LedToggle(LED_3);
+		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	}
 }
-/*==================[end of file]============================================*/
